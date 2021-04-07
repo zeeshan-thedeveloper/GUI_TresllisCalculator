d71546551f20bff13a07ca7e7877d2958f892977
Zeeshan <66442918+ZeeTheGamer@users.noreply.github.com>
2021-04-07 16:54:38 +0500

    List For creating node is done

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -9,10 +11,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class GUIController implements Initializable{
-	
+	private static String selectedChoice;
 	@FXML
 	TextField identificationN;
 	@FXML
@@ -23,9 +26,31 @@ public class GUIController implements Initializable{
 	Button openFileExploral;
 	@FXML 
 	ComboBox<Support_Type> comboBox_support;
-	public void test() {
-		System.out.print("Here"+txt_abscisse.getText());
+	@FXML
+	ListView<Node_DataHolder> node_list;
+	
+	public void create_node() {
+	
+		int choice =0;
+		if(selectedChoice.equals("Support unique"))
+		{
+			choice=1;
+		}
+		else if(selectedChoice.equals("Double soutien"))
+		{
+			choice=2;
+		}
+		else {
+			choice =3;
+		}
+		
+		//This method will create an object will store in the list.
+		node_list.getItems().add(new Node_DataHolder(identificationN.getText(),txt_abscisse.getText(),txt_ordonne.getText(), choice));
+	
 	}
+	
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -33,10 +58,11 @@ public class GUIController implements Initializable{
 			    new Support_Type("Pas de support"),
 			    new Support_Type("Support unique"), 
 			    new Support_Type("Double soutien")));
-
 		comboBox_support.valueProperty().addListener((obs, oldVal, newVal) -> 
-			    System.out.println("Selected type is :" + newVal.getType()));
+			    selectedChoice =  newVal.getType());
 	}
+	
+	
 	
 	
 }
@@ -60,4 +86,63 @@ class Support_Type{
 		// TODO Auto-generated method stub
 		return type;
 	}
+}
+class Node_DataHolder
+{
+	
+	String identificationN,txt_abscisse,txt_ordonne;
+	int type_support;
+	public Node_DataHolder(String identificationN, String txt_abscisse,String txt_ordonne,int type_support) {
+		// TODO Auto-generated constructor stub
+		this.identificationN=identificationN;
+		this.txt_abscisse=txt_abscisse;
+		this.txt_ordonne=txt_abscisse;
+		this.type_support=type_support;
+	}
+	public String getIdentificationN() {
+		return identificationN;
+	}
+	public void setIdentificationN(String identificationN) {
+		this.identificationN = identificationN;
+	}
+	public String getTxt_abscisse() {
+		return txt_abscisse;
+	}
+	public void setTxt_abscisse(String txt_abscisse) {
+		this.txt_abscisse = txt_abscisse;
+	}
+	public String getTxt_ordonne() {
+		return txt_ordonne;
+	}
+	public void setTxt_ordonne(String txt_ordonne) {
+		this.txt_ordonne = txt_ordonne;
+	}
+	public int getType_support() {
+		return type_support;
+	}
+	public void setType_support(int type_support) {
+		this.type_support = type_support;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String ty = "";
+		if(type_support==1)
+		{
+			ty="Support unique";
+		}
+		else if(type_support==2)
+		{
+			ty="Double soutien";
+		}
+		else
+		{
+			ty = "Pas de support";
+		}
+		
+		return identificationN+" | x :"+txt_abscisse+" | y :"+txt_ordonne+" | type : "+ty; 
+	}
+	
+	
 }
